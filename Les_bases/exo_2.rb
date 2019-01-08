2b9cacaf36ee2b7791427a112370be1d7d281077
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-08 08:32:02 +0300

    ajout des bases

---
+++ Les_bases/exo_2.rb
@@ -0,0 +1,31 @@
+# The way of declaration of variable in ruby
+variable = "programming"
+count = 10
+gpa_madagascar = 12.1 
+puts "#{count} #{variable} #{gpa_madagascar}"
+count = count + 2
+count += 2
+count = 4 *16
+count = 34**4
+print("The Last count is: #{count}\n")
+# Creation of upercase and dowcase in ruby
+title = "Matrix"
+puts title.upcase
+puts title.upcase!
+puts title.downcase!
+# Concatenisation in ruby between string
+titleMovie = "The Lord of the Ring"
+titleMovie += ": The Return of the King"
+puts titleMovie
+# Concatenisation with the multiplication
+language = "Ruby "
+puts language * 3
+puts "And Still\n"
+puts language * 7
+# Using of arguments in ruby for all 
+# to create an argument is: #{...}
+puts "#{title} #{titleMovie}"
+# CONVENTION FOR THE CONSTANT IN RUBY
+CONSTANT = 6
+NOMBRE_CONSTANT = 9.19
+puts "Nos constantes sont:\n#{CONSTANT}\nEt #{NOMBRE_CONSTANT}"
---
+++ Les_bases/family.rb
@@ -0,0 +1,13 @@
+# Ages of family members
+mom = 48
+dad = 51
+john = 18
+mary = 16
+
+# Sum of the age of each one of the member of the family
+age_sum = mom + dad + john + mary
+puts "The sum of the age of each family member is:\n#{age_sum}"
+
+# The result of the calcul
+result = (mom * dad) / (john - mary)
+puts "The result of the calculation is:\n#{result}"
